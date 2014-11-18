af12179abc823adbd10bf0b9b67a4a0b56aec780
James Strachan <james.strachan@gmail.com>
2014-11-18 14:37:10 +0000

    migrate the mq-producer and mq-consumer to use the new camel-amq component

--- apps/fabric8-mq-consumer/src/main/java/io/fabric8/mq/consumer/ConsumerMain.java
+++ apps/fabric8-mq-consumer/src/main/java/io/fabric8/mq/consumer/ConsumerMain.java
@@ -15,8 +15,6 @@
  */
 package io.fabric8.mq.consumer;
 
-import io.fabric8.utils.Systems;
-import org.apache.activemq.camel.component.ActiveMQComponent;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.dataset.SimpleDataSet;
 import org.slf4j.Logger;
@@ -28,32 +26,13 @@ import java.security.PrivilegedAction;
 public class ConsumerMain {
     private static final Logger LOG = LoggerFactory.getLogger(ConsumerMain.class);
 
-    public static final String DEFAULT_HOST = "127.0.0.1";
-    public static final String DEFAULT_PORT = "61616";
-
-    private static String host;
-
-    private static int port;
-    
     private static int prefetch;
-    
+
     private static String queueName;
 
     public static void main(String args[]) {
         try {
-            String serviceName = Systems.getEnvVarOrSystemProperty("AMQ_SERVICE_ID", "AMQ_SERVICE_ID", "FABRIC8MQ").toUpperCase() + "_SERVICE";
-            String hostEnvVar = serviceName + "_HOST";
-            String portEnvVar = serviceName + "_PORT";
             try {
-
-                host = Systems.getEnvVarOrSystemProperty(hostEnvVar, hostEnvVar, DEFAULT_HOST);
-                String portStr = Systems.getEnvVarOrSystemProperty(portEnvVar, hostEnvVar, DEFAULT_PORT);
-                if (portStr != null && portStr.length() > 0) {
-                    port = Integer.parseInt(portStr);
-                }
-                if (portStr != null && portStr.length() > 0) {
-                    port = Integer.parseInt(portStr);
-                }
                 String prefetchStr = AccessController.doPrivileged(new PrivilegedAction<String>() {
                     @Override
                     public String run() {
@@ -63,9 +42,9 @@ public class ConsumerMain {
                     }
                 });
                 if (prefetchStr != null && prefetchStr.length() > 0) {
-                	prefetch = Integer.parseInt(prefetchStr);
+                    prefetch = Integer.parseInt(prefetchStr);
                 }
-                
+
                 queueName = AccessController.doPrivileged(new PrivilegedAction<String>() {
                     @Override
                     public String run() {
@@ -74,54 +53,35 @@ public class ConsumerMain {
                         return result;
                     }
                 });
-                
+
             } catch (Throwable e) {
                 LOG.warn("Failed to look up System properties for host and port", e);
             }
 
-            if (host == null || host.length() == 0) {
-                host = DEFAULT_HOST;
-            }
-
-            if (port <= 0) {
-                port = 61616;
-            }
-            
             if (prefetch <= 0) {
-            	prefetch = 1000;
-            }
-            
-            if (queueName == null) {
-            	queueName = "TEST.FOO";
+                prefetch = 1000;
             }
 
-            System.out.println("Using broker host " + host + " from $" + hostEnvVar + " and port " + port + " from $" + portEnvVar);
+            if (queueName == null) {
+                queueName = "TEST.FOO";
+            }
 
             // create a camel route to consume messages from our queue
             org.apache.camel.main.Main main = new org.apache.camel.main.Main();
-            String brokerURL = "failover://(tcp://" + host + ":" + port + ")";
-            System.out.println("Connecting to brokerURL " + brokerURL);
-            main.bind("activemq", ActiveMQComponent.activeMQComponent(brokerURL));
             main.bind("myDataSet", new SimpleDataSet());
             main.enableHangupSupport();
-            
+
             main.addRouteBuilder(new RouteBuilder() {
                 public void configure() {
-                    
-            		from("activemq:"+queueName).
-            		to("dataset:myDataSet");
+                    from("amq:" + queueName).to("dataset:myDataSet");
                 }
             });
-            
+
             main.run(args);
 
         } catch (Throwable e) {
-            LOG.error("Failed to connect to Fabric8MQ", e);
+            LOG.error("Failed to connect to Fabric8 MQ", e);
         }
-    }
-
-    public static int getPort() {
-        return port;
     }
 
     public static String getQueueName() {
--- apps/fabric8-mq-producer/src/main/java/io/fabric8/mq/producer/ProducerMain.java
+++ apps/fabric8-mq-producer/src/main/java/io/fabric8/mq/producer/ProducerMain.java
@@ -15,8 +15,6 @@
  */
 package io.fabric8.mq.producer;
 
-import io.fabric8.utils.Systems;
-import org.apache.activemq.camel.component.ActiveMQComponent;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.dataset.DataSet;
 import org.apache.camel.component.dataset.SimpleDataSet;
@@ -29,34 +27,18 @@ import java.util.Arrays;
 
 public class ProducerMain {
     private static final Logger LOG = LoggerFactory.getLogger(ProducerMain.class);
-    public static final String DEFAULT_HOST = "127.0.0.1";
-    public static final String DEFAULT_PORT = "61616";
-
-    private static String host;
-
-    private static int port;
 
     private static int interval;
-    
+
     private static String queueName;
-    
+
     private static int messageSize;
 
     private static long messageCount;
 
     public static void main(String args[]) {
         try {
-            String serviceName = Systems.getEnvVarOrSystemProperty("AMQ_SERVICE_ID", "AMQ_SERVICE_ID", "FABRIC8MQ").toUpperCase() + "_SERVICE";
-            String hostEnvVar = serviceName + "_HOST";
-            String portEnvVar = serviceName + "_PORT";
             try {
-
-                host = Systems.getEnvVarOrSystemProperty(hostEnvVar, hostEnvVar, DEFAULT_HOST);
-                String portStr = Systems.getEnvVarOrSystemProperty(portEnvVar, hostEnvVar, DEFAULT_PORT);
-                if (portStr != null && portStr.length() > 0) {
-                    port = Integer.parseInt(portStr);
-                }
-
                 queueName = AccessController.doPrivileged(new PrivilegedAction<String>() {
                     @Override
                     public String run() {
@@ -65,7 +47,7 @@ public class ProducerMain {
                         return result;
                     }
                 });
-                
+
                 String intervalStr = AccessController.doPrivileged(new PrivilegedAction<String>() {
                     @Override
                     public String run() {
@@ -75,114 +57,95 @@ public class ProducerMain {
                     }
                 });
                 if (intervalStr != null && intervalStr.length() > 0) {
-                	interval = Integer.parseInt(intervalStr);
+                    interval = Integer.parseInt(intervalStr);
                 }
 
                 String messageSizeInBytesStr = AccessController.doPrivileged(new PrivilegedAction<String>() {
-                	@Override
-                	public String run() {
-                		String result = System.getenv("AMQ_MESSAGE_SIZE_BYTES");
-                		result = (result == null || result.isEmpty()) ? System.getProperty("org.apache.activemq.AMQ_MESSAGE_SIZE_BYTES", "1024") : result;
-                		return result;
-                	}
+                    @Override
+                    public String run() {
+                        String result = System.getenv("AMQ_MESSAGE_SIZE_BYTES");
+                        result = (result == null || result.isEmpty()) ? System.getProperty("org.apache.activemq.AMQ_MESSAGE_SIZE_BYTES", "1024") : result;
+                        return result;
+                    }
                 });
                 if (messageSizeInBytesStr != null && messageSizeInBytesStr.length() > 0) {
-                	messageSize = Integer.parseInt(messageSizeInBytesStr);
+                    messageSize = Integer.parseInt(messageSizeInBytesStr);
                 }
 
                 String messageCountStr = AccessController.doPrivileged(new PrivilegedAction<String>() {
-                	@Override
-                	public String run() {
-                		String result = System.getenv("AMQ_MESSAGE_COUNT_LONG");
-                		result = (result == null || result.isEmpty()) ? System.getProperty("org.apache.activemq.AMQ_MESSAGE_COUNT_LONG", "10000") : result;
-                		return result;
-                	}
+                    @Override
+                    public String run() {
+                        String result = System.getenv("AMQ_MESSAGE_COUNT_LONG");
+                        result = (result == null || result.isEmpty()) ? System.getProperty("org.apache.activemq.AMQ_MESSAGE_COUNT_LONG", "10000") : result;
+                        return result;
+                    }
                 });
                 if (messageCountStr != null && messageCountStr.length() > 0) {
-                	messageCount= Long.parseLong(messageCountStr);
+                    messageCount = Long.parseLong(messageCountStr);
                 }
-                
+
             } catch (Throwable e) {
                 LOG.warn("Failed to look up System properties for host and port", e);
             }
 
-            if (host == null || host.length() == 0) {
-                host = DEFAULT_HOST;
-            }
-            if (port <= 0) {
-                port = 61616;
+            if (queueName == null) {
+                queueName = "TEST.FOO";
             }
 
-            if (queueName == null) {
-            	queueName = "TEST.FOO";
-            }
-            
             if (messageSize <= 1) {
-            	messageSize = 1024;
+                messageSize = 1024;
             }
 
             if (messageCount <= 0) {
-            	messageCount = 10000;
+                messageCount = 10000;
             }
-    		
-            System.out.println("Using broker host " + host + " from $" + hostEnvVar + " and port " + port + " from $" + portEnvVar);
+
 
             // create a camel route to produce messages to our queue
             org.apache.camel.main.Main main = new org.apache.camel.main.Main();
 
-            String brokerURL = "failover://(tcp://" + host + ":" + port+")";
-            System.out.println("Connecting to brokerURL: " + brokerURL);
-
-            main.bind("activemq", ActiveMQComponent.activeMQComponent(brokerURL));
             main.bind("myDataSet", createDataSet());
-            
+
             main.enableHangupSupport();
-            
+
             main.addRouteBuilder(new RouteBuilder() {
                 public void configure() {
-                    
-                	from("dataset:myDataSet?produceDelay="+interval).
-                	to("activemq:"+queueName);
+                    from("dataset:myDataSet?produceDelay=" + interval).to("amq:" + queueName);
                 }
             });
-            
+
             main.run(args);
         } catch (Throwable e) {
             LOG.error("Failed to connect to Fabric8 MQ", e);
         }
     }
 
-	static DataSet createDataSet() {
-
+    static DataSet createDataSet() {
         char[] chars = new char[messageSize];
         Arrays.fill(chars, 'a');
-            
+
         String messageBody = new String(chars);
 
         SimpleDataSet dataSet = new SimpleDataSet();
-		dataSet.setSize(messageCount);
-		dataSet.setDefaultBody(messageBody);
+        dataSet.setSize(messageCount);
+        dataSet.setDefaultBody(messageBody);
 
-		return dataSet;
-	}
-    
-    public static int getPort() {
-        return port;
+        return dataSet;
     }
 
     public static int getInterval() {
         return interval;
     }
-    
+
     public static String getQueueName() {
         return queueName;
     }
 
-	public static int getMessageSize() {
-		return messageSize;
-	}
+    public static int getMessageSize() {
+        return messageSize;
+    }
 
-	public static long getMessageCount() {
-		return messageCount;
-	}
+    public static long getMessageCount() {
+        return messageCount;
+    }
 }
