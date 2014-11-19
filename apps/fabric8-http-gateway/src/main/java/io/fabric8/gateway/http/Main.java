93f7ebb2607db1705c052cbb879279d3d00079d1
Kurt Stam <kstam@redhat.com>
2014-11-19 09:38:37 -0500

    adding apiman config

--- apps/fabric8-http-gateway/src/main/java/io/fabric8/gateway/http/Main.java
+++ apps/fabric8-http-gateway/src/main/java/io/fabric8/gateway/http/Main.java
@@ -15,7 +15,6 @@
  */
 package io.fabric8.gateway.http;
 
-import io.fabric8.gateway.fabric.http.FabricHTTPGateway;
 import io.fabric8.gateway.fabric.http.HTTPGatewayConfig;
 import io.fabric8.gateway.fabric.http.HttpMappingRuleConfiguration;
 import io.fabric8.gateway.loadbalancer.LoadBalancers;
@@ -34,13 +33,13 @@ public class Main {
     public static final String DEFAULT_HOST = "127.0.0.1";
     public static final String DEFAULT_PORT = "9090";
     public static final String DEFAULT_INDEX_ENABLED = "true";
-    public static final String DEFAULT_GATEWAY_SERVICES_SELECTOR = "[{\"container\":\"java\",\"group\":\"quickstarts\"},{\"container\":\"camel\",\"group\":\"quickstarts\"}]";
+    public static final String DEFAULT_GATEWAY_SERVICES_SELECTORS = "[{\"container\":\"java\",\"group\":\"quickstarts\"},{\"container\":\"camel\",\"group\":\"quickstarts\"}]";
     public static final String DEFAULT_URI_TEMPLATE = "/{contextPath}";
     public static final String DEFAULT_LOADBALANCER = LoadBalancers.ROUND_ROBIN_LOAD_BALANCER;
     public static final String DEFAULT_KUBERNETES_MASTER = "http://localhost:8484/";
+    public static final String DEFAULT_API_MANAGER_ENABLED = "true";
     private static final Logger LOG = LoggerFactory.getLogger(Main.class);
     
-    @Inject FabricHTTPGateway fabricHTTPGateway;
     @Inject HttpMappingRuleConfiguration httpMappingRuleConfigutation;
     @Inject @Parameters String[] args;
 
@@ -50,8 +49,9 @@ public class Main {
         String serviceName = Systems.getEnvVarOrSystemProperty("HTTP_GATEWAY_SERVICE_ID", "HTTP_GATEWAY_SERVICE_ID", "FABRIC8HTTPGATEWAY").toUpperCase() + "_SERVICE_";
         String hostEnvVar = serviceName + HTTPGatewayConfig.HOST;
         String portEnvVar = HTTPGatewayConfig.HTTP_PORT;
+        String apiManagerEnabledEnvVar = serviceName + HTTPGatewayConfig.IS_API_MANAGER_ENABLED;
         String kubernetesMasterEnvVar = HTTPGatewayConfig.KUBERNETES_MASTER;
-        String selectorEnvVar = serviceName + HTTPGatewayConfig.SELECTOR;
+        String selectorEnvVar = serviceName + HTTPGatewayConfig.SELECTORS;
         String uriTemplateEnvVar = serviceName + HTTPGatewayConfig.URI_TEMPLATE;
         String loadBalancerEnvVar = serviceName + HTTPGatewayConfig.LOAD_BALANCER;
         String enabledVersionEnvVar = serviceName + HTTPGatewayConfig.ENABLED_VERSION;
@@ -63,16 +63,19 @@ public class Main {
                 Systems.getEnvVarOrSystemProperty(portEnvVar, portEnvVar, DEFAULT_PORT));
         gatewayConfig.put(HTTPGatewayConfig.ENABLE_INDEX,
                 Systems.getEnvVarOrSystemProperty(portEnvVar, portEnvVar, DEFAULT_INDEX_ENABLED));
+        gatewayConfig.put(HTTPGatewayConfig.IS_API_MANAGER_ENABLED,
+        		Systems.getEnvVarOrSystemProperty(apiManagerEnabledEnvVar, apiManagerEnabledEnvVar, DEFAULT_API_MANAGER_ENABLED));
         LOG.info("Container host " + gatewayConfig.getHost());
         LOG.info("Container port " + gatewayConfig.getPort());
         LOG.info("Index enabled: " + gatewayConfig.isIndexEnabled());
+        LOG.info("API Manager Enabled: " + gatewayConfig.isApiManagerEnabled());
         //Kube config
         gatewayConfig.put(HTTPGatewayConfig.KUBERNETES_MASTER,
-        		Systems.getEnvVarOrSystemProperty(kubernetesMasterEnvVar, kubernetesMasterEnvVar, DEFAULT_KUBERNETES_MASTER));
+                Systems.getEnvVarOrSystemProperty(kubernetesMasterEnvVar, kubernetesMasterEnvVar, DEFAULT_KUBERNETES_MASTER));
         LOG.info("Kubernetes Master: " + gatewayConfig.getKubernetesMaster());
         //Rule config
-        gatewayConfig.put(HTTPGatewayConfig.SELECTOR,
-                Systems.getEnvVarOrSystemProperty(selectorEnvVar, selectorEnvVar, DEFAULT_GATEWAY_SERVICES_SELECTOR));
+        gatewayConfig.put(HTTPGatewayConfig.SELECTORS,
+                Systems.getEnvVarOrSystemProperty(selectorEnvVar, selectorEnvVar, DEFAULT_GATEWAY_SERVICES_SELECTORS));
         gatewayConfig.put(HTTPGatewayConfig.URI_TEMPLATE,
                 Systems.getEnvVarOrSystemProperty(uriTemplateEnvVar, uriTemplateEnvVar, DEFAULT_URI_TEMPLATE));
         gatewayConfig.put(HTTPGatewayConfig.LOAD_BALANCER,
@@ -81,10 +84,15 @@ public class Main {
                 Systems.getEnvVarOrSystemProperty(enabledVersionEnvVar, enabledVersionEnvVar, null));
         gatewayConfig.put(HTTPGatewayConfig.REVERSE_HEADERS,
                 Systems.getEnvVarOrSystemProperty(reverseHeadersEnvVar, reverseHeadersEnvVar, "true"));
-        
+        LOG.info("Gateway Selector: " + gatewayConfig.get(HTTPGatewayConfig.SELECTORS));
+        LOG.info("Gateway URI Template; " + gatewayConfig.get(HTTPGatewayConfig.URI_TEMPLATE));
+        LOG.info("LoadBalancer: " + gatewayConfig.getLoadBalancerType());
+        LOG.info("Enabled Version: " + gatewayConfig.getEnabledVersion());
+        LOG.info("Reverse Headers: " + gatewayConfig.isReverseHeaders());
+        LOG.info("Pushing configuration to the gateway...");
         try {
             httpMappingRuleConfigutation.configure(gatewayConfig);
-            fabricHTTPGateway.configure(gatewayConfig);
+            LOG.info("Gateway startup successful");
             waitUntilStop();
         } catch (Exception e) {
             LOG.error("Could not start the HTTP Gateway");
