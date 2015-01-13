2849944488f59f2472de192479e63519f4665512
Jimmi Dyson <jimmidyson@gmail.com>
2015-01-13 11:28:24 +0000

    Re-enable spring-boot camel quickstart

---
+++ quickstarts/spring-boot/camel/src/main/java/io/fabric8/quickstarts/camel/App.java
@@ -0,0 +1,12 @@
+package io.fabric8.quickstarts.camel;
+
+import org.springframework.boot.SpringApplication;
+
+public class App {
+
+    public static void main(String[] args) {
+        new SpringApplication(args).run();
+    }
+
+
+}
--- quickstarts/spring-boot/camel/src/main/java/io/fabric8/quickstarts/camel/CamelConfig.java
+++ quickstarts/spring-boot/camel/src/main/java/io/fabric8/quickstarts/camel/CamelConfig.java
@@ -16,29 +16,38 @@
 package io.fabric8.quickstarts.camel;
 
 import org.apache.camel.CamelContext;
-import org.apache.camel.RoutesBuilder;
 import org.apache.camel.builder.RouteBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.camel.component.metrics.routepolicy.MetricsRoutePolicyFactory;
+import org.apache.camel.component.properties.PropertiesComponent;
+import org.apache.camel.spring.javaconfig.CamelConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class CamelConfiguration {
+public class CamelConfig extends CamelConfiguration {
 
     @Bean
-    RoutesBuilder routeBuilder() {
+    public RouteBuilder route() {
         return new RouteBuilder() {
             @Override
             public void configure() throws Exception {
-                from("timer://foo?fixedRate=true&period=5000").
+                from("timer://foo?fixedRate=true&period=5000&daemon=false").
                         setBody().constant("<hello>world!</hello>").
                         log(">>> ${body}");
             }
         };
     }
 
-    // Collaborators fixtures
-    @Autowired
-    CamelContext camelContext;
+    @Override
+    protected void setupCamelContext(CamelContext camelContext) throws Exception {
+        // make Camel aware of Spring Boot’s application.properties
+        PropertiesComponent pc = new PropertiesComponent();
+        pc.setLocation("classpath:application.properties");
+        camelContext.addComponent("properties", pc);
 
+        // enable performance metrics
+        camelContext.addRoutePolicyFactory(new MetricsRoutePolicyFactory());
+
+        super.setupCamelContext(camelContext);
+    }
 }
