f49da286663215b7abe19621821caa51897fb68c
Ioannis Canellos <iocanel@gmail.com>
2015-03-09 16:17:10 +0200

    Fix infinispan-server app.

--- apps/infinispan-server/src/main/resources/bin/start.sh
+++ apps/infinispan-server/src/main/resources/bin/start.sh
@@ -5,7 +5,8 @@ cp -rf /opt/jboss/upload/standalone /opt/jboss/infinispan-server/
 
 export JAVA_OPTS="-Djava.net.preferIPv4Stack=true"
 export KUBERNETES_TRUST_CERT="true"
-export DOCKER_IP=$(ip addr show eth0 | grep -E '^\s*inet' | grep -m1 global | awk '{ print $2 }' | sed 's|/.*||')
+export IP=$(ip addr show eth0 | grep -E '^\s*inet' | grep -m1 global | awk '{ print $2 }' | sed 's|/.*||')
 export export KUBERNETES_TRUST_CERT="true"
 
-/bin/sh /opt/jboss/infinispan-server/bin/clustered.sh -b $DOCKER_IP -bmanagement $DOCKER_IP -c clustered.xml
+echo "Starting Clustered Infinispan Server using Docker Address: $IP"
+sh /opt/jboss/infinispan-server/bin/clustered.sh -b $IP -bmanagement $IP -c clustered.xml
