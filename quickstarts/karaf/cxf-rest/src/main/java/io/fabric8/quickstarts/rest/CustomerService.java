53159c972ed27ccc59be8f1cc6ebb3612ff7d54c
Kurt Stam <kstam@redhat.com>
2015-09-21 14:45:30 -0400

    upgrading to io.swagger 1.5.3 to support Swagger 2.0

--- quickstarts/karaf/cxf-rest/src/main/java/io/fabric8/quickstarts/rest/CustomerService.java
+++ quickstarts/karaf/cxf-rest/src/main/java/io/fabric8/quickstarts/rest/CustomerService.java
@@ -28,11 +28,11 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
 
-import com.wordnik.swagger.annotations.Api;
-import com.wordnik.swagger.annotations.ApiOperation;
-import com.wordnik.swagger.annotations.ApiParam;
-import com.wordnik.swagger.annotations.ApiResponses;
-import com.wordnik.swagger.annotations.ApiResponse;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.ApiResponse;
 
 import org.apache.cxf.jaxrs.ext.MessageContext;
 import org.slf4j.Logger;
