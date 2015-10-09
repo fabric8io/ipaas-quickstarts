1f857b605dcc083d0ec091828d153751ad956720
Claus Ibsen <claus.ibsen@gmail.com>
2015-10-09 16:06:30 +0200

    rename as kube service must be 24 or less

--- quickstart/cdi/camel-http/src/test/resources/hitme-f8.sh
+++ quickstart/cdi/camel-http/src/test/resources/hitme-f8.sh
@@ -2,7 +2,7 @@
 echo "Hit ctrl+c to stop"
 while :
 do
-	curl curl http://qs-cdi-camel-jetty.vagrant.f8/camel/hello?name=donald
+	curl http://qs-cdi-camel-jetty.vagrant.f8/camel/hello?name=donald
 	echo ""
     sleep 1
 done
