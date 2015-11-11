249bfb8944c97003a3c97e0d2273d3302dd9c87b
Dhiraj Bokde <dbokde@redhat.com>
2015-11-10 23:31:40 -0800

    OSFUSE-106: Temporarily disabled itest for cdi-camel-mq due to archetype generation issue, basically archetypes are generated before quickstarts are rebuilt

--- quickstart/cdi/camel-mq/src/main/java/io/fabric8/quickstarts/camelcdi/mq/ActiveMQConfig.java
+++ quickstart/cdi/camel-mq/src/main/java/io/fabric8/quickstarts/camelcdi/mq/ActiveMQConfig.java
@@ -13,7 +13,7 @@
  * implied.  See the License for the specific language governing
  * permissions and limitations under the License.
  */
-package io.fabric8.mq;
+package io.fabric8.quickstarts.camelcdi.mq;
 
 import org.apache.deltaspike.core.api.config.ConfigProperty;
 
