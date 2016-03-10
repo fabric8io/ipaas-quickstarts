422f16d3ae795cf0a5cc4d07c22cc8fff806b9e5
Claus Ibsen <claus.ibsen@gmail.com>
2016-03-10 09:48:40 +0100

    Polished vertx example

--- quickstart/vertx/simplest/src/main/java/io/vertx/example/HelloWorldEmbedded.java
+++ quickstart/vertx/simplest/src/main/java/io/vertx/example/HelloWorldEmbedded.java
@@ -2,21 +2,26 @@ package io.vertx.example;
 
 import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpServer;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
 
 public class HelloWorldEmbedded {
 
     public static void main(String[] args) {
         // enable logging
         System.setProperty("vertx.logger-delegate-factory-class-name", "io.vertx.core.logging.Log4jLogDelegateFactory");
+        final Logger logger = LoggerFactory.getLogger("my-logger");
 
         final String podName = System.getenv("HOSTNAME") != null ? System.getenv("HOSTNAME") : "localhost";
 
         // Create an HTTP server which simply returns "Hello World!" to each request.
         HttpServer server = Vertx.vertx().createHttpServer()
-            .requestHandler(req ->
-                req.response().end("Hello World! from pod: " + podName + "\n"));
+            .requestHandler(req -> {
+                logger.info("Processing incoming request");
+                req.response().end("Hello World! from pod: " + podName + "\n");
+            });
 
-        System.out.println("vert.x HTTP server running on port 8080");
+        logger.info("vert.x HTTP server running on port 8080");
         // start server which automatic closes when jvm terminates
         server.listen(8080);
     }
