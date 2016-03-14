fe05af4dc49b0c0ef3aeaf20039314c5de0ee6dc
Claus Ibsen <claus.ibsen@gmail.com>
2016-03-14 17:12:18 +0100

    Disable this as hawt-app causes this to add the jolokia agent a 2nd time, but the k8s base images already have jolokia.

--- quickstart/cdi/camel/src/main/hawt-app/bin/setenv.sh
+++ quickstart/cdi/camel/src/main/hawt-app/bin/setenv.sh
@@ -1,3 +1,4 @@
 # Enable the jolokia agent configured using etc/jolokia.properties
-JAVA_OPTIONS="${JAVA_OPTIONS} -javaagent:agents/jolokia.jar=config=etc/jolokia.properties"
-export JAVA_OPTIONS
+# TODO: Does not work when deploying to k8s as the base image already have jolokia
+# JAVA_OPTIONS="${JAVA_OPTIONS} -javaagent:agents/jolokia.jar=config=etc/jolokia.properties"
+# export JAVA_OPTIONS
