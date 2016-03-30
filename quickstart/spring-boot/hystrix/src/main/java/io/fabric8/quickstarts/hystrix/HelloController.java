15bfe8306c17ecf6ca2fd66dea42f0a7820d4283
James Strachan <james.strachan@gmail.com>
2016-03-30 16:51:14 +0100

    added a vanilla spring-boot hystrix quickstart without ribbon client side discovery or client side load balancing for folks who don't want the extra cost, jars and complexity

---
+++ quickstart/spring-boot/hystrix/src/main/java/io/fabric8/quickstarts/hystrix/HelloController.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2015 Red Hat, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.fabric8.quickstarts.hystrix;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class HelloController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping("/hello")
+    @HystrixCommand(fallbackMethod = "helloFallback", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "5000")
+    })
+    public String hello() {
+        return restTemplate.getForObject("http://hello-service/hello", String.class);
+    }
+
+    public String helloFallback() {
+        return "Hello Fallback";
+    }
+}
