23358d24c3890a2b261dd637ab251f5440bc94e3
rawlingsj <rawlingsj80@gmail.com>
2016-04-11 20:49:36 +0100

    add release pipeline

---
+++ release.groovy
@@ -0,0 +1,51 @@
+#!/usr/bin/groovy
+def updateDependencies(source){
+
+  def properties = []
+  properties << ['<fabric8.version>','io/fabric8/kubernetes-api']
+  properties << ['<docker.maven.plugin.version>','io/fabric8/docker-maven-plugin']
+
+  updatePropertyVersion{
+    updates = properties
+    repository = source
+    project = 'fabric8io/ipaas-quickstarts'
+  }
+}
+
+def stage(){
+  return stageProject{
+    project = 'fabric8io/ipaas-quickstarts'
+    useGitTagForNextVersion = true
+  }
+}
+
+def approveRelease(project){
+  def releaseVersion = project[1]
+  approve{
+    room = null
+    version = releaseVersion
+    console = null
+    environment = 'fabric8'
+  }
+}
+
+def release(project){
+  releaseProject{
+    stagedProject = project
+    useGitTagForNextVersion = true
+    helmPush = false
+    groupId = 'io.fabric8.archetypes'
+    githubOrganisation = 'fabric8io'
+    artifactIdToWatchInCentral = 'archetypes-catalog'
+    artifactExtensionToWatchInCentral = 'jar'
+  }
+}
+
+def mergePullRequest(prId){
+  mergeAndWaitForPullRequest{
+    project = 'fabric8io/ipaas-quickstarts'
+    pullRequestId = prId
+  }
+
+}
+return this;
